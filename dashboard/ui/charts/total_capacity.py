146e5c5bfa7c9730dba7b4f17a7fb1d9f9e9b619
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-06-11 17:34:02 +0200

    chore: move header to top

--- dashboard/ui/charts/total_capacity.py
+++ dashboard/ui/charts/total_capacity.py
@@ -2,14 +2,14 @@ import streamlit as st
 from plots.technology_bar import plot_tot_bars
 
 def render_total_capicity(data):
+    st.subheader("Installed capacity by technology")
+    
     tot = data["var_tot_pcap"].copy()
     new = data["var_new_pcap"].copy()
 
     combined = tot.merge(new, on="g", how="left", suffixes=("_tot", "_new"))
     combined["value_new"] = combined["value_new"].fillna(0)
     combined["existing"] = combined["value_tot"] - combined["value_new"]
-
-    st.subheader("Installed capacity by technology")
 
     view = st.radio("Show", ["Existing + New", "Total only", "New only"], horizontal=True)
 
