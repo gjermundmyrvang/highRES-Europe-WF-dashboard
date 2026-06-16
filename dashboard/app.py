6f208aa72ee35ec2d5c1c9a14f10d4f5be8b2fba
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-06-16 12:46:45 +0200

    chore: clean up app.py and use new function to set the focused scenario

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,32 +1,11 @@
 import streamlit as st
 import json
-from pathlib import Path
-from data_loader import find_result_files 
+from data_loader import find_work_folders 
 from data.loader import load_scenario
 from ui.sidebar import render_sidebar
 from ui.charts.total_capacity import render_total_capicity
+from ui.charts.zone_capacity_v2 import render_zone_capicity_v2
 from ui.charts.zone_capacity import render_zone_capicity
-import argparse
-
-TECH_COLORS = {
-    "Solar":               "#F5C518",
-    "Windonshore":         "#4A90D9",
-    "Windoffshore":        "#1B5FA8",
-    "WindoffshoreFloat":   "#0D3B6E",
-    "HydroRoR":            "#2ECC71",
-    "HydroRes":            "#1A8C4E",
-    "NuclearEPR":          "#E74C3C",
-}
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--results-path", type=str, default=None)
-args = parser.parse_args()
-
-if args.results_path:
-    scenario_paths = {Path(args.results_path).name: Path(args.results_path) / "results.gdx"}
-else:
-    scenario_paths = find_result_files()
-
 
 # ---------- PAGE SETUP ------------
 st.set_page_config(layout="wide")
@@ -34,22 +13,27 @@ st.title("highRES Dashboard")
 st.caption("Energy system model results (page under development...)")
 
 def main():
-    scenario = render_sidebar(scenario_paths)
+    scenario = render_sidebar(find_work_folders("./")) # Load focused scenario
 
-    data = load_scenario(scenario_paths[scenario])
+    data = load_scenario(scenario / "results.gdx")
 
-    col1, col2 = st.columns(2)
+    total_cost = data["costs"].iloc[0]["value"]
+    st.subheader(f"Total Cost: {round(total_cost, 3)}")
+
 
     # --------------- CHARTS (Aggregated for all countries) ---------
-    with col1:
-        render_total_capicity(data)
+    render_total_capicity(data)
+    st.divider()
         
-    # -------------- CHARTS (By zones) ------------------
+    # -------------- Zone Map ------------------
     with open("intermediate_data/region/shapes/europe_onshore.geojson") as f:
         geo = json.load(f)
+    render_zone_capicity(data, geo)
+    st.divider()
 
-    with col2:
-        render_zone_capicity(data, geo)
+    # -------------- Installed vs. Potential ------------------
+    render_zone_capicity_v2(data)
+    st.divider()
     
 if __name__ == "__main__":
     main()
