dc354cf09c8b83048063962306e508662fcabe1b
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-06-16 12:48:36 +0200

    chore: change from manual filter to just use built in filter functions

--- dashboard/plots/technology_bar.py
+++ dashboard/plots/technology_bar.py
@@ -1,11 +1,12 @@
 import plotly.express as px
 
-def plot_tot_bars(df, view, value_col):
+def plot_tot_bars(df):
     fig = px.bar(
         df,
         x="g",
-        y=value_col[view],
+        y=["existing", "value_new"],
         labels={"g": "Technology", "value": "Capacity (GW)", "variable": "Type"},
         height=700,
+        width=900
     )
     return fig
