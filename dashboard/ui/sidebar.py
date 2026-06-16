881f37402c4086e5d48ecee6e6f2e24840ab094b
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-06-16 12:45:43 +0200

    chore: update sidebar to display both work folders and scenarios inside

--- dashboard/ui/sidebar.py
+++ dashboard/ui/sidebar.py
@@ -1,7 +1,18 @@
 import streamlit as st
 
-def render_sidebar(scenario_paths):
+def render_sidebar(work_folders: dict):
     with st.sidebar:
         st.header("Settings")
-        selected = st.selectbox("Scenario", options=list(scenario_paths.keys()))
-        return selected
+
+        selected_folder = st.selectbox(
+            "Folder",
+            options=list(work_folders.keys()),
+        )
+
+        scenarios = work_folders[selected_folder]
+        selected_scenario = st.selectbox(
+            "Scenario",
+            options=list(scenarios.keys()),
+        )
+
+        return scenarios[selected_scenario]
