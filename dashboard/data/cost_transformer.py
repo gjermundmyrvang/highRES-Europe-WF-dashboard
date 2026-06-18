835c98796da13e001f1e6f6cf4731207160dee16
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-06-18 16:27:04 +0200

    feat: add functions for adjusting for inflation and currency

---
+++ dashboard/data/cost_transformer.py
@@ -0,0 +1,17 @@
+import requests
+
+def get_eur_gbp_rate():
+    url = "https://api.frankfurter.dev/v2/rates"
+    headers = {"Accept": "application/json"}
+    params = {"base": "GBP", "quotes": "EUR"}
+
+    response = requests.get(url, headers=headers, params=params)
+    parsed = response.json()
+    return parsed[0]["rate"]
+
+def adjust_inflation(gbp_value):
+    return gbp_value * 1.586
+
+def adjust_currency(gbp_value, rate):
+    adjusted = gbp_value * rate
+    return adjusted
