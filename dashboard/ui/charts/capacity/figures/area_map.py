519e03dc6a0ded67946517a7bf3e210832f19c43
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-06-22 15:16:20 +0200

    feat: add area map with click to select functionality (used to trigger details section)

---
+++ dashboard/ui/charts/capacity/figures/area_map.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import plotly.express as px
+
+def render_area_map(df, geo):
+    fig = px.choropleth(
+        df,
+        geojson=geo,
+        locations="z",
+        featureidkey="properties.index",
+        color="utilization_pct",
+        color_continuous_scale="Greens",
+        hover_name="z",
+        hover_data={
+            "z": False,
+            "potential_area": ":.1f",
+            "installed_area": ":.1f",
+            "utilization_pct": ":.1f",
+        },
+        title="VRE area utilization by country (%)",
+        labels={
+        "utilization_pct": "Utilization (%)",
+        "potential_area": "Potential area (km²)",
+        "installed_area": "Installed area (km²)",
+    },
+    )
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(height=800)
+    
+    selected = st.plotly_chart(fig, on_select="rerun",use_container_width="stretch", key="map") # Workaround for click-to-select
+
+    return selected
