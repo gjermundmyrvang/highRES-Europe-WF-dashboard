66f17222060207eb0eba595d835bfb97e0ee5d4c
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-06-22 11:50:08 +0200

    chore: move techcolors to data directory

---
+++ dashboard/data/tech_colors.py
@@ -0,0 +1,9 @@
+TECH_COLORS = {
+    "Solar":               "#F5C518",
+    "Windonshore":         "#4A90D9",
+    "Windoffshore":        "#1B5FA8",
+    "WindoffshoreFloat":   "#0D3B6E",
+    "HydroRoR":            "#2ECC71",
+    "HydroRes":            "#1A8C4E",
+    "NuclearEPR":          "#E74C3C",
+}
