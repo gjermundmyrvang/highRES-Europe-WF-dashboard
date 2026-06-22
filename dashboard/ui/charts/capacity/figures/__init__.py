f8980a65d2a81e4d630d3c684f0d935b2d5c1115
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-06-22 15:16:57 +0200

    chore: structure imports

--- dashboard/ui/charts/capacity/figures/__init__.py
+++ dashboard/ui/charts/capacity/figures/__init__.py
@@ -1,3 +1,5 @@
 from .sankey import render_sankey
 from .pivot import render_pivot
 from .bar_chart import render_bar_chart
+from .area_map import render_area_map
+from .country_charts import render_country_charts
