6b6879458e75bd1b5bcadaf9ee63effed6077c4a
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-06-22 11:55:38 +0200

    chore: clean up app.py with new tab structure

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import json
 from data_loader import find_work_folders 
 from data.loader import load_scenario
-from data.cost_transformer import adjust_currency, adjust_inflation, get_eur_gbp_rate
-from ui.sidebar import render_sidebar
+from ui import render_sidebar, render_system_metrics
 from ui.charts import render_total_capicity, render_zone_capicity, render_zone_capicity_v2, render_capacity_charts
 
 
@@ -17,29 +16,29 @@ def main():
 
     data = load_scenario(scenario / "results.gdx")
 
-    total_cost = data["costs"].iloc[0]["value"]
-    st.subheader(f"Model Raw Cost: {round(total_cost)} (in million GBP)")
+    tab1, tab2 = st.tabs(["Overview", "Explore Capacity"])
 
-    adjusted = adjust_currency(adjust_inflation(total_cost * 1000000), get_eur_gbp_rate())
-    st.subheader(f"Total Cost: €{adjusted:,.2f}")
-    st.text("Converted to millions --> adjusted for inflation --> adjusted for currency translation")
+    with tab1:
+        render_system_metrics(data)
 
-    # --------------- CHARTS (Aggregated for all countries) ---------
-    render_total_capicity(data)
-    st.divider()
-        
-    # -------------- Zone Map ------------------
-    with open("intermediate_data/region/shapes/europe_onshore.geojson") as f:
-        geo = json.load(f)
-    render_zone_capicity(data, geo)
-    st.divider()
+        # --------------- CHARTS (Aggregated for all countries) ---------
+        render_total_capicity(data)
+        st.divider()
+            
+        # -------------- Zone Map ------------------
+        with open("intermediate_data/region/shapes/europe_onshore.geojson") as f:
+            geo = json.load(f)
+        render_zone_capicity(data, geo)
+        st.divider()
 
-    # -------------- Installed vs. Potential ------------------
-    # render_zone_capicity_v2(data)
-    # st.divider()
+        # -------------- Installed vs. Potential ------------------
+        # render_zone_capicity_v2(data)
+        # st.divider()
 
-    # ------------ EXPLORATION OF CHARTS -------------------
-    render_capacity_charts(data)
+    with tab2:
+
+        # ------------ EXPLORATION OF CAPACITY CHARTS -------------------
+        render_capacity_charts(data)
     
 if __name__ == "__main__":
     main()
