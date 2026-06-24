34818c976b9adbc63b29650f1f48882a1e352a50
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-06-24 13:14:27 +0200

    chore: provide meta data to overview tab

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import json
-from data_loader import find_work_folders 
+from data_loader import find_work_folders, load_sets 
 from data.loader import load_scenario
 from ui import render_sidebar
 from ui.tabs import render_overview, render_capacity
@@ -15,13 +15,15 @@ def main():
     scenario = render_sidebar(find_work_folders("./")) # Load focused scenario
 
     data = load_scenario(scenario / "results.gdx")
+    hour_data = load_sets(scenario / "results.gdx")
+
     with open("intermediate_data/region/shapes/europe_onshore.geojson") as f:
         geo = json.load(f)
 
     tab1, tab2, tab3 = st.tabs(["Overview", "Explore Capacity", "Map"])
 
     with tab1:
-        render_overview(data)
+        render_overview(data, hour_data)
 
     with tab2:
         render_capacity(data, geo)
