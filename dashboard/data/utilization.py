1811ae1fba24f195f0e7791816d0628c69f93d92
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-06-30 11:44:54 +0200

    chore: fix utilization function to include flag wether to map gw to km2 values

--- dashboard/data/utilization.py
+++ dashboard/data/utilization.py
@@ -3,35 +3,45 @@ import numpy as np
 # TODO: This is different between techs?
 AREA_FACTOR = 2.4  # 1 km² produces 2.4 GW?
 
+CAPACITY_TO_AREA = {
+    "Solar": 0.04,
+    "Windoffshore": 0.005,
+    "Windonshore": 0.0024,
+    "HydroRoR": 0.001,
+}
 
-def calculate_utilization(tot_z, potential_z):
-    vre_techs = set(potential_z["g"].unique()) # area-table only has VRE techs
-    installed_vre = tot_z[tot_z["g"].isin(vre_techs)]
- 
+
+def calculate_utilization(new_vre_z, potential_z, use_area=False):
+    potential_z = potential_z[potential_z["g"] != "HydroRoR"]  # Exclude (INF+)
+    vre_techs = set(potential_z["g"].unique())  # area-table only has VRE techs
+    installed_vre = new_vre_z[new_vre_z["g"].isin(vre_techs)]
+
     installed_agg = (
-        installed_vre
-        .groupby(["z", "g"], as_index=False)["value"]
+        installed_vre.groupby(["z", "g"], as_index=False)["value"]
         .sum()
-        .rename(columns={"value": "installed"}) # For code clarity later on
+        .rename(columns={"value": "installed"})  # For code clarity later on
     )
- 
+
     potential_agg = (
-        potential_z
-        .groupby(["z", "g"], as_index=False)["value"]
+        potential_z.groupby(["z", "g"], as_index=False)["value"]
         .sum()
-        .rename(columns={"value": "potential"}) # For code clarity later on
+        .rename(columns={"value": "potential"})  # For code clarity later on
     )
- 
+
     df = potential_agg.merge(installed_agg, on=["z", "g"], how="left")
     df["installed"] = df["installed"].fillna(0)
- 
+
+    if use_area:
+        df["installed"] = df["installed"] / df["g"].map(CAPACITY_TO_AREA)
+        df["potential"] = df["potential"] / df["g"].map(CAPACITY_TO_AREA)
+
     # If potential > 0, calculate utilization as installed/potential * 100. Otherwise set it to NaN
     df["utilization_pct"] = np.where(
         df["potential"] > 0,
-        df["installed"] / df["potential"] * 100,
+        (df["installed"] / df["potential"] * 100),
         np.nan,
     )
- 
+
     return df
 
 
