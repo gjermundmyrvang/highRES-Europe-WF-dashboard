3fd1f3e216649d3c42cb4a4ded8b56a1c9629a58
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-06-30 11:42:51 +0200

    chore: move techcolors to designated constants file

---
+++ dashboard/data/constants.py
@@ -0,0 +1,28 @@
+TECH_ICONS = {
+    "Solar": ":material/sunny:",
+    "Windoffshore": ":material/air:",
+    "Windonshore": ":material/air:",
+    "HydroRoR": ":material/water:",
+    "HydroRes": ":material/water:",
+    "NaturalgasCCGTwithCCSnewOT": ":material/local_fire_department:",
+    "NaturalgasOCGTnew": ":material/local_fire_department:",
+    "Biomass": ":material/grass:",
+    "BiomassCCS": ":material/grass:",
+    "NuclearEPR": ":material/radio_button_checked:",
+    "SynCon": ":material/settings_input_component:",
+}
+
+TECH_COLORS = {
+    "Solar": "#F5C518",
+    "Windonshore": "#4A90D9",
+    "Windoffshore": "#1B5FA8",
+    "WindoffshoreFloat": "#0D3B6E",
+    "HydroRoR": "#2ECC71",
+    "HydroRes": "#1A8C4E",
+    "NuclearEPR": "#E74C3C",
+    "NaturalgasCCGTwithCCSnewOT": "#7F8C8D",
+    "NaturalgasOCGTnew": "#95A5A6",
+    "Biomass": "#8E6E53",
+    "BiomassCCS": "#6B4F3A",
+    "SynCon": "#9B59B6",
+}
