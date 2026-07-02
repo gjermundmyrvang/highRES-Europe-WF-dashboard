56592f8f9204fcc21e9ec42753e5c86b7b5448fc
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-07-02 10:58:45 +0200

    chore: remove unused code

--- dashboard/ui/tabs/capacity/figures/__init__.py
+++ dashboard/ui/tabs/capacity/figures/__init__.py
@@ -1,7 +1,6 @@
 from .sankey import render_sankey
 from .pivot import render_pivot
 from .bar_chart import render_bar_chart
-from .area_map import render_area_map
 from .country_charts import render_country_charts
 from .scatter_plot import render_country_scatter
 from .pie_chart import render_capacity_pies
