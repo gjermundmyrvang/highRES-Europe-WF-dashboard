5fa89dea6e4a875ede47d4fcdd3cdd0ba8ca0353
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-07-20 13:00:03 +0200

    feat: create function to calculate country areas from geojson file

--- dashboard/data/transformer.py
+++ dashboard/data/transformer.py
@@ -1,8 +1,9 @@
-def build_zone_capacity(tot_z, new_z):
-    total = tot_z.groupby(["z", "g"])["value"].sum().reset_index(name="total")
-    new = new_z.groupby(["z", "g"])["value"].sum().reset_index(name="new")
+import geopandas as gpd
 
-    df = total.merge(new, on=["z", "g"], how="left")
-    df["new"] = df["new"].fillna(0)
-    df["existing"] = df["total"] - df["new"]
-    return df
+
+def load_country_areas(geojson_path: str) -> dict[str, float]:
+    gdf = gpd.read_file(geojson_path)
+    # EPSG: 3035: The official standard for pan-European statistical analysis, land use (from google search)
+    gdf = gdf.to_crs("EPSG:3035")
+    gdf["area_km2"] = gdf.geometry.area / 1e6  # 10**6
+    return dict(zip(gdf["index"], gdf["area_km2"]))
