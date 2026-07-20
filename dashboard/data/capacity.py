20ae236676ab2535d6543f08aaa490d6ac29b752
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-07-20 12:37:21 +0200

    chore: change from hardcoded dict to values from gdx set

--- dashboard/data/capacity.py
+++ dashboard/data/capacity.py
@@ -1,12 +1,14 @@
-from data.constants import VRE_TECHS
+from data_loader import table
 
 
-def capacity_summary(df):
+def capacity_summary(df, sets):
+    vre_table = table(sets, "vre")
+    vre_techs = list(vre_table["g"])
     total_all = df["var_tot_pcap"]
     new_all = df["var_new_pcap"]
 
-    installed_vre = total_all[total_all["g"].isin(VRE_TECHS)]
-    installed_new_vre = new_all[new_all["g"].isin(VRE_TECHS)]
+    installed_vre = total_all[total_all["g"].isin(vre_techs)]
+    installed_new_vre = new_all[new_all["g"].isin(vre_techs)]
 
     return {
         "total_installed": total_all["value"].sum().round(0),
