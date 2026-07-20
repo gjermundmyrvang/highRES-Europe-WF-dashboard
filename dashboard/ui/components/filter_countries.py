18496de2089ecf2390a9339ca9a21d7e8ed5ebd5
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-07-20 12:50:16 +0200

    chore: make default choice handling more implicit

--- dashboard/ui/components/filter_countries.py
+++ dashboard/ui/components/filter_countries.py
@@ -1,8 +1,10 @@
 import streamlit as st
 
 
-def filter_countries(df, default=[], key="filter_countries"):
+def filter_countries(df, default=None, key="filter_countries"):
     all_countries = sorted(set(df["country_name"]))
+    if default is None:
+        default = all_countries[:5]
     selected_countries = st.multiselect(
         "Filter countries",
         options=all_countries,
