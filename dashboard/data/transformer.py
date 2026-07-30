1662548ea2e854621c0a24bd93700405dfd3a44e
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-07-30 12:29:36 +0200

    chore: move function from scenarions tab to transformer now that its shared

--- dashboard/data/transformer.py
+++ dashboard/data/transformer.py
@@ -1,3 +1,5 @@
+import random
+
 import geopandas as gpd
 
 
@@ -7,3 +9,13 @@ def load_country_areas(geojson_path: str) -> dict[str, float]:
     gdf = gdf.to_crs("EPSG:3035")
     gdf["area_km2"] = gdf.geometry.area / 1e6  # 10**6
     return dict(zip(gdf["index"], gdf["area_km2"]))
+
+
+def random_hex_color(seed_str):
+    """
+    TODO:
+    -----
+    This is used in `render_capacity_mix` so dimensions with other 'techs' thats does not have a color dict also gets a unique color. However random hex can produce colors that are too dark, too light, or low-contrast against the dashboard background. A better solution should be implemented.
+    """
+    rng = random.Random(seed_str)
+    return "#{:06x}".format(rng.randint(0, 0xFFFFFF))
